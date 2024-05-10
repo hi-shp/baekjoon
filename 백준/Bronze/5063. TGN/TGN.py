7be0f63b0438a08d3579d092bddd31bd09e4e1cb
hi-shp <168538598+hi-shp@users.noreply.github.com>
2024-05-10 21:43:47 +0900

    [Bronze III] Title: TGN, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,9 @@
+T = int(input())
+for i in range(T):
+    a,b,c = map(int,input().split())
+    if a < b-c:
+        print('advertise')
+    elif a == b-c:
+        print('does not matter')
+    else:
+        print('do not advertise')
