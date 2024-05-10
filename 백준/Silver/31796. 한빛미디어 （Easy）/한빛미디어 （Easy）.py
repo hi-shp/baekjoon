5f91a3d30038d66daaad9b43c552e7b0ad16d0cd
hi-shp <168538598+hi-shp@users.noreply.github.com>
2024-05-11 01:55:31 +0900

    [Silver IV] Title: 한빛미디어 (Easy), Time: 152 ms, Memory: 122620 KB -BaekjoonHub

--- 백준/Silver/31796. 한빛미디어 （Easy）/한빛미디어 （Easy）.py
+++ 백준/Silver/31796. 한빛미디어 （Easy）/한빛미디어 （Easy）.py
@@ -1,17 +1,15 @@
-import sys
-input = sys.stdin.readline
 N = int(input())
 if N != 1:
     a = list(map(int, input().split()))
     a.sort()
     t = []
     x = a[0]
-    k = 0
+    k = 1
     for i in range(N):
         if a[i] >= 2*x:
             k += 1
             x = a[i]
     t.append(k)
-    print(max(t)+1)
+    print(min(t))
 else:
     print(1)
