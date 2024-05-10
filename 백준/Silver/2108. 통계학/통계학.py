786e007d4fc9b451141a1596b4fb17dec4ec4b61
hi-shp <168538598+hi-shp@users.noreply.github.com>
2024-05-10 23:14:48 +0900

    [Silver III] Title: 통계학, Time: 2896 ms, Memory: 122844 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,19 @@
+T = int(input())
+a = [int(input()) for _ in range(T)]
+print(round(sum(a)/T))
+a = sorted(a)
+print(round(a[int((T-1)/2)]))
+k = max(a) - min(a)
+b = set(a)
+b = list(b)
+cnt = [a.count(x) for x in b]
+if cnt.count(max(cnt)) > 1:
+    c = []
+    for i in range(len(cnt)):
+        if cnt[i] == max(cnt):
+            c.append(b[i])
+    c = sorted(set(c))
+    print(c[1])
+else:
+    print(b[cnt.index(max(cnt))])
+print(k)
