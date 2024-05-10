95bf7a13a1a586be752ddd6097439738c85f6233
hi-shp <168538598+hi-shp@users.noreply.github.com>
2024-05-10 21:46:34 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a >= 90 :
+    print('A')
+elif 80 < a < 90 or a == 80:
+    print('B')
+elif 70 < a < 80 or a == 70:
+    print('C')
+elif 60 < a < 70 or a == 60:
+    print('D')
+else :
+    print('F')
